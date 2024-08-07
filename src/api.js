9edd349e1189762bec3845942ec4d075b10f1e53
kimyeonji <hfgj0014@naver.com>
2024-08-07 17:37:55 +0900

    api 설정 중간저장

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "http://apis.data.go.kr/1262000/CountryHistoryService2/getCountryHistoryList2"
+const option = {
+    method: "GET",
+    headers: {
+        accept: "application/json"
+        Authorization:"	czjc%2FLiXADgQwbo%2BD7wPVIDHrQsrdTly%2FtPY1ISDPgq1b4XfyXD4WryP5J6TNaEsZF9d%2F0%2F10Cto7gia8HcDnA%3D%3D"
+    }
+}
